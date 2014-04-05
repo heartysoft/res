c2af032e8c13f80fe77cf9ea9d0b250b41dad737
ashic <ashic@live.com>
2014-04-05 14:52:09 +0100

    added host shell + logging

---
+++ src/res/Res/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Topshelf;
+using Topshelf.Common.Logging;
+
+namespace Res
+{
+    class Program
+    {
+        private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                Logger.Error("Hello start");
+                x.Service<ResHost>(s =>
+                {
+                    s.ConstructUsing(name => new ResHost());
+                    s.WhenStarted(rh => rh.Start());
+                    s.WhenStopped(rh => rh.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Res");
+                x.SetDisplayName("Res");
+                x.SetServiceName("Res");
+
+                x.UseCommonLogging();
+            });
+        }
+    }
+}
---
+++ src/res/Res/ResHost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Common.Logging;
+
+namespace Res
+{
+    public class ResHost
+    {
+        private static ILog _logger = LogManager.GetCurrentClassLogger();
+        public void Start()
+        {
+            Console.WriteLine("Started.");
+            _logger.Error("Whhoop");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopped");
+            _logger.Info("deee");
+        }
+    }
+}
