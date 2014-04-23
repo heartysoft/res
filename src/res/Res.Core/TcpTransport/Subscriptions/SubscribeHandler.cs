28060d9c3b7069872fb731f9d759ca7b2f776915
Ashic Mahtab (MC) <ashic.mahtab@moneycorp.com>
2014-04-23 14:14:20 +0100

    slight refactoring...nothing to see here...

--- src/res/Res.Core/TcpTransport/Subscriptions/SubscribeHandler.cs
+++ src/res/Res.Core/TcpTransport/Subscriptions/SubscribeHandler.cs
@@ -34,21 +34,12 @@ namespace Res.Core.TcpTransport.Subscriptions
             var subscriberId = message.Pop().ConvertToString();
             var count = int.Parse(message.Pop().ConvertToString());
 
-            var requests = new SubscribeRequest[count];
+            var responses = doRequest(message, count, subscriberId);
+            sendResponse(sender, requestId, count, responses);
+        }
 
-            for (var i = 0; i < count; i++)
-            {
-                var context = message.Pop().ConvertToString();
-                var filter = message.Pop().ConvertToString();
-                var startTime = DateTime.FromBinary(long.Parse(message.Pop().ConvertToString()));
-                var now = DateTime.UtcNow;
-
-                var request = new SubscribeRequest(i, subscriberId, context, filter, startTime, now);
-                requests[i] = request;
-            }
-
-            var responses = _storage.Subscribe(requests);
-
+        private void sendResponse(NetMQFrame[] sender, NetMQFrame requestId, int count, SubscribeResponse[] responses)
+        {
             var msg = new NetMQMessage();
 
             msg.Append(sender);
@@ -63,10 +54,27 @@ namespace Res.Core.TcpTransport.Subscriptions
             }
 
             var completed = new SubscribeCompleted(msg);
-            while(!_outBuffer.Offer(completed))
+            while (!_outBuffer.Offer(completed))
                 _spin.SpinOnce();
-            
         }
-        
+
+        private SubscribeResponse[] doRequest(NetMQMessage message, int count, string subscriberId)
+        {
+            var requests = new SubscribeRequest[count];
+
+            for (var i = 0; i < count; i++)
+            {
+                var context = message.Pop().ConvertToString();
+                var filter = message.Pop().ConvertToString();
+                var startTime = DateTime.FromBinary(long.Parse(message.Pop().ConvertToString()));
+                var now = DateTime.UtcNow;
+
+                var request = new SubscribeRequest(i, subscriberId, context, filter, startTime, now);
+                requests[i] = request;
+            }
+
+            var responses = _storage.Subscribe(requests);
+            return responses;
+        }
     }
 }
