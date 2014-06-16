b97187a55bd8258889a5c7be3dc8ed834dc06c91
ashic <ashic@live.com>
2014-06-16 14:08:07 +0100

    started with queues... Protocol seems ready.

---
+++ src/res/Res.Core/TcpTransport/Queues/SubscribeHandler.cs
@@ -0,0 +1,34 @@
+﻿using Common.Logging;
+using NetMQ;
+using Res.Core.TcpTransport.MessageProcessing;
+using Res.Core.TcpTransport.NetworkIO;
+
+namespace Res.Core.TcpTransport.Queues
+{
+    public interface QueueStorage
+    {
+    }
+
+    public class SubscribeHandler : RequestHandler
+    {
+        private readonly QueueStorage _storage;
+        private readonly OutBuffer _outBuffer;
+        private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
+
+        public SubscribeHandler(QueueStorage storage, OutBuffer outBuffer)
+        {
+            _storage = storage;
+            _outBuffer = outBuffer;
+        }
+
+        public void Handle(NetMQFrame[] sender, NetMQMessage message)
+        {
+            Logger.Debug("[Queue_SubscribeHandler] Received subscribe request.");
+
+            var requestId = message.Pop();
+            
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
